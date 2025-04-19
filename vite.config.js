ff410750aae4d262077add6ec58ffd65739bc458
droser <droser@cs.washington.edu>
2025-04-19 15:09:15 -0700

    changed vite config and built

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CodeRacer/', // <-- MUST MATCH your GitHub repo name exactly
   plugins: [react()],
 })
